fc08eb9e5bee9feb37c4bb7f6dade186bac32fe5
gengauss <shiroemonmadeon@gmail.com>
2020-08-06 01:03:41 +0900

    designing contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,56 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+import avatar from '../avatar.png'
+
 
 class Contact extends Component {
     render() {
         return(
-            <div><h1>Contact Page</h1></div>
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2>Anh Nguyen</h2>
+                        <img
+                            src={avatar}
+                            alt="avatar"
+                            style={{height: '250px'}}
+                        />
+                        <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr/>
+
+                        <div className="contact-list">
+                            <List>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
+                                        <i className="fa fa-phone-square" aria-hidden="true"/>
+                                        (123) 456-7890
+                                    </ListItemContent>
+                                </ListItem>
+
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
+                                        <i className="fa fa-envelope" aria-hidden="true"/>
+
+                                    </ListItemContent>
+                                </ListItem>
+
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
+                                        <i className="fa fa-skype" aria-hidden="true"/>
+                                        MySkypeID
+                                    </ListItemContent>
+                                </ListItem>
+
+
+                            </List>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
