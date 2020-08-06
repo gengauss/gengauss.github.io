5a3bae2ad1917f711ef411c54b1dbc559e3af512
gengauss <shiroemonmadeon@gmail.com>
2020-08-07 00:36:04 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content} from "react-mdl";
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main';
 import { Link } from 'react-router-dom';
 
-function App() {
-    return (
-        <div className="demo-big-content">
-            <Layout>
-                <Header className="header-color" title={<a href="/me">ANH NGUYEN</a>}>
-                    <Navigation>
-                        <Link to="/resume">Resume</Link>
-                        <Link to="/aboutme">About Me</Link>
-                        <Link to="/projects">Projects</Link>
-                        <Link to="/contact">Contact</Link>
-                    </Navigation>
-                </Header>
-                <Drawer className="header-color" title={<a href="/me">ANH NGUYEN</a>}>
-                    <Navigation>
-                        <Link to="/resume">Resume</Link>
-                        <Link to="/aboutme">About Me</Link>
-                        <Link to="/projects">Projects</Link>
-                        <Link to="/contact">Contact</Link>
-                    </Navigation>
-                </Drawer>
-                <Content>
-                    <div className="page-content" />
-                    <Main/>
-                </Content>
-            </Layout>
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <div className="demo-big-content">
+                <Layout>
+                    <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">ANH NGUYEN</Link>} scroll>
+                        <Navigation>
+                            <Link to="/resume">Resume</Link>
+                            <Link to="/aboutme">About Me</Link>
+                            <Link to="/projects">Projects</Link>
+                            <Link to="/contact">Contact</Link>
+                        </Navigation>
+                    </Header>
+                    <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">ANH NGUYEN</Link>}>
+                        <Navigation>
+                            <Link to="/resume">Resume</Link>
+                            <Link to="/aboutme">About Me</Link>
+                            <Link to="/projects">Projects</Link>
+                            <Link to="/contact">Contact</Link>
+                        </Navigation>
+                    </Drawer>
+                    <Content>
+                        <div className="page-content" />
+                        <Main/>
+                    </Content>
+                </Layout>
+            </div>
+
+        );
+    }
 }
 
 export default App;
