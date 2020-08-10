26812939f2c99e29ccf5a0eefefef8d7a2fb819b
gengauss <shiroemonmadeon@gmail.com>
2020-08-10 10:21:05 +0900

    fix opacity

--- src/components/index.js
+++ src/components/index.js
@@ -6,8 +6,7 @@ import {makeStyles} from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
     particlesCanva: {
-        position: "fixed",
-        opacity: "0.5"
+        position: "fixed"
     }
 })
 
