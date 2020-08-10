b8cbdb3651fb26d99fc790a46f7ee794251a0aec
gengauss <shiroemonmadeon@gmail.com>
2020-08-10 18:17:39 +0900

    Updates

---
+++ precache-manifest.8deab0e9cd332eedbf16ed83375c68de.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70702e6c4f43d08e63b8a2266faaf3b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfd64e50e46ed14cd32a",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "ba3620728e6a31c9c90a",
+    "url": "/static/css/main.14ac54d7.chunk.css"
+  },
+  {
+    "revision": "dfd64e50e46ed14cd32a",
+    "url": "/static/js/2.7177aeb6.chunk.js"
+  },
+  {
+    "revision": "4233e42ae1d14a4f0f23cb066c59a9a3",
+    "url": "/static/js/2.7177aeb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba3620728e6a31c9c90a",
+    "url": "/static/js/main.363dfacf.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "e25f06a69e42f16577963b4c493eafc8",
+    "url": "/static/media/avatar.e25f06a6.png"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/static/media/react-redux.a8b012a0.jpg"
+  }
+]);
